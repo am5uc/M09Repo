c279838f77468df95e5c02f10067719ce748c125
Abhinandan <am5uc@virginia.edu>
2023-10-24 20:46:33 -0400

    M09 Exercise Files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='m09_package',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/am5uc/M09Repo',
+    author='Abhinandan Mekap',
+    author_email='am5uc@virginia.edu',
+    license='MIT',
+    packages=['m09_package']
+)
